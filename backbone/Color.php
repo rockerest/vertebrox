6f399e11969ea088e723f55d064ad601e05266a7
Tom Randolph <websites@thomasrandolph.info>
2011-01-25 16:48:08 -0600

    Started creating Color class.
    This isn't really anything important, I'm just checking that I can commit files that are beyond a symlink.

--- backbone/Color.php
+++ backbone/Color.php
@@ -1,8 +1,21 @@
 <?php
 	class Color
 	{
-		public function __construct()
+		private $hex;
+		private $r;
+		private $g;
+		private $b;
+		
+		public function __construct( $a, $b = null, $c = null )
 		{
+			if( $b == null || $c == null )
+			{
+				//hex, or unknown input
+			}
+			elseif( $a == null )
+			{
+				//bad input
+			}
 		}
 	}
 ?>
