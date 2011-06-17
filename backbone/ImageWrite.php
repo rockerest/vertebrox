94b1c9ede031b1b5658e0a6133283fcde8c71bae
Tom Randolph <websites@thomasrandolph.info>
2011-06-17 17:19:25 -0500

    + added code to overlay the text not where you wanted it, but rather a location so that it will APPEAR to be where you wanted it.

--- backbone/ImageWrite.php
+++ backbone/ImageWrite.php
@@ -48,9 +48,67 @@
 				$textRot = $text->Manipulate->Rotate($angle);
 				if( $textRot )
 				{
-					if( $this->caller->Combine->Overlay( $textRot, $x, $y, 0, 0, $textRot->width, $textRot->height ) )
+					//get dimensions
+					$Ox = $textRot->width;
+					$Oy = $textRot->height;
+					$tx = $text->width;
+					$ty = $text->width;
+					
+					//make angle safe
+					$a = $angle;
+					while( $a < 0 || $a >= 360 )
 					{
-						$this->caller->Draw->Rectangle($x, $y, $textRot->width, $textRot->height, "#FF0000");
+						if( $a < 0 )
+						{
+							$a += 360;
+						}
+						
+						if( $a >= 360 )
+						{
+							$a -= 360;
+						}
+					}
+					
+					$deltaX = 0;
+					$deltaY = 0;
+					
+					//calculate re-anchor point based on quadrant
+					if( $a >= 0 && $a <= 90 )
+					{
+						$deltaX = 0;
+						$deltaY = -(sin(deg2rad($a)) * $tx);
+					}
+					elseif( $a > 90 && $a <= 180 )
+					{
+						$deltaX = -(sin(deg2rad(90 - (180 - $a))) * $tx);
+						$deltaY = -$Oy;
+					}
+					elseif( $a > 180 && $a <= 270 )
+					{
+						$deltaX = -$Ox;
+						$deltaY = -(sin(deg2rad(270 - $a)) * $ty);
+					}
+					elseif( $a > 270 && $a < 360 )
+					{
+						$deltaX = -(sin(deg2rad(360 - $a)) * $ty);
+						$deltaY = 0;
+					}
+					
+					//if the offset places the image outside of the original, crop until it fits
+					//if cropped, reset the coordinate to 0
+					$newx = $x + round($deltaX);
+					$newy = $y + round($deltaY);
+					
+					$left = $newx < 0 ? abs($newx) : 0;
+					$top = $newy < 0 ? abs($newy) : 0;
+					
+					$xp = $newx < 0 ? 0 : $newx;
+					$yp = $newy < 0 ? 0 : $newy;
+
+					$cropFit = $textRot->Manipulate->Crop($top, 0, 0, $left);
+					
+					if( $this->caller->Combine->Overlay( $cropFit, $xp, $yp, 0, 0, $cropFit->width, $cropFit->height ) )
+					{
 						return true;
 					}
 					else
