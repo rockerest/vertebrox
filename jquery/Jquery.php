c381685b61ae9ae8e3c4f6ac39c6b8cc7ca28c05
Tom Randolph <websites@thomasrandolph.info>
2011-12-21 17:05:12 -0600

    ~ added .nudge and .push to positioning to give a left and right margin - removed static html jquery loader + added Jquery.php to dynamically load jquery script tags

---
+++ jquery/Jquery.php
@@ -0,0 +1,31 @@
+<?php
+	class Jquery
+	{
+		private $jquery;
+		private $jqueryui;
+		public function __construct()
+		{
+			$v = "1.7"; //jQuery version
+			$uiv = "1.8.16"; // jQueryUI version
+			
+			$this->jquery = "<!--    Load Javascript Library - jQuery	-->\n<script type=\"text/javascript\" src=\"https://ajax.googleapis.com/ajax/libs/jquery/$v/jquery.min.js\"></script>";
+			$this->jqueryui = "<!--	Load jQuery User Interface		-->\n<script type=\"text/javascript\" src=\"https://ajax.googleapis.com/ajax/libs/jqueryui/$uiv/jquery-ui.min.js\"></script>";
+		}
+		
+		public function __get($var)
+		{
+			if( $var == "jquery" )
+			{
+				return $this->jquery . "\n";
+			}
+			elseif( $var == "jqueryui" || $var == "ui" )
+			{
+				return $this->jqueryui . "\n";
+			}
+			elseif( $var == "both" || $var == "all" || $var == null || !isset($var) )
+			{
+				return $this->jquery . "\n" . $this->jqueryui . "\n";
+			}
+		}
+	}
+?>
