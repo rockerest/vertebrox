20c3d86b83f8042ad1237297d681e32703384245
Tom Randolph <websites@thomasrandolph.info>
2012-06-04 22:54:58 -0500

    ~ Cleaner syntax (no drop braces) + All cookies use the right parameters (set the params before any cookie functions)

--- backbone/Session.php
+++ backbone/Session.php
@@ -1,29 +1,26 @@
 <?php
-	function setSession($expire = 0, $root = '/', $dom = null, $secure = false, $httponly = true)
-	{
-		if( $dom == null )
-		{
+	function setSession($expire = 0, $root = '/', $dom = null, $secure = false, $httponly = true){
+		if( $dom == null ){
 			$dom = '.' . $_SERVER['HTTP_HOST'];
 		}
 		
-		if( !isset( $_COOKIE['identifier'] ) )
-		{
+		session_set_cookie_params( $expire, $root, $dom, $secure, $httponly );
+
+		if( !isset( $_COOKIE['identifier'] ) ){
 			$myTempVar = uniqid(TRUE);
 			$sesswhirl = substr( hash('whirlpool', $myTempVar), 0, 68 );
-		
-			setcookie('identifier', $sesswhirl, $expire, $root, $dom, $secure, $httponly);
+
+			setcookie('identifier', $sesswhirl);
 			session_name($sesswhirl);
 			session_start();
 		}
-		else
-		{
+		else{
 			session_name($_COOKIE['identifier']);
 			session_start();
 		}
 	}
 	
-	function setSessionVar($name, $value)
-	{
+	function setSessionVar($name, $value){
 		$_SESSION[$name] = $value;
 	}
 ?>
